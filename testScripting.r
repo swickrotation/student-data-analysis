413edfefdb0d058ab39ad5afdc0ea5c99ea0dafd
swickrotation <will.gertler@gmail.com>
2023-01-04 16:44:53 -0500

    further code and data cleanup

--- testScripting.r
+++ testScripting.r
@@ -6,24 +6,25 @@ library(readODS)
 #Loading in working data from working directory. Will require an edit depending
 #on the placement of the working data files in the experimenter's filesystem.
 #For us, the working git repo is located in ~/gitrepos/student-data-analysis and
-#its associated subdirectories.
+#its associated subdirectories, being workingData, from which we load individual
+#files.
 
-onex_att <- read_ods(path="~/gitrepos/student-data-analysis/workingData/1x-att.ods",sheet=1)
-twoz_att <- read_ods(path="~/gitrepos/student-data-analysis/workingData/2z-att.ods",sheet=1)
-#onex_gra <- read_ods(path="~/gitrepos/student-data-analysis/workingData/1x-gra.ods",sheet=1)
-#twoz_gra <- read_ods(path="~/gitrepos/student-data-analysis/workingData/2z-gra.ods",sheet=1)
+classOne_attendance <- read_ods(path="~/gitrepos/student-data-analysis/workingData/1x-att.ods",sheet=1)
+classTwo_attendance <- read_ods(path="~/gitrepos/student-data-analysis/workingData/2z-att.ods",sheet=1)
+classOne_grades <- read_ods(path="~/gitrepos/student-data-analysis/workingData/1x-gra.ods",sheet=1)
+classTwo_grades <- read_ods(path="~/gitrepos/student-data-analysis/workingData/2z-gra.ods",sheet=1)
 
 
 #Compute new statistics: Attendance (TO BE GENERALIZED WITH ID VARIABLES)
 #
 #
 #Daily Averages (this is just for me, this is probably junk code)
-dailyMeanAttendance_onex<-data.frame(colMeans(onex_att[c(6:length(onex_att))], na.rm=T))
-dailyMeanAttendance_twoz<-data.frame(colMeans(twoz_att[c(6:length(twoz_att))], na.rm=T))
+dailyMeanAttendance_classOne<-data.frame(colMeans(classOne_attendance[c(6:length(classOne_attendance))], na.rm=T))
+dailyMeanAttendance_classTwo<-data.frame(colMeans(classTwo_attendance[c(6:length(classTwo_attendance))], na.rm=T))
 
 #For now, these next two lines take the average of the attendance calculated
 #from line 6 and out to the ends of their respective data frames, appending
 #a new column on the end of the data frame with the new statistic for average
 #attendance by student.
-onex_att$meanAttendance <- rowMeans(onex_att[,c(6:length(onex_att))], na.rm=TRUE)
-twoz_att$meanAttendance <- rowMeans(twoz_att[,c(6:length(twoz_att))], na.rm=TRUE)
+meanAttendanceByStudent_classOne <- data.frame(rowMeans(classOne_attendance[,c(6:length(classOne_attendance))], na.rm=TRUE))
+meanAttendanceByStudent_classTwo <- data.frame(rowMeans(classTwo_attendance[,c(6:length(classTwo_attendance))], na.rm=TRUE))
