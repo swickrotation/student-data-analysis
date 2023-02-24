50a4ae7c32b696c16be76bafe657ddee812bec32
swickrotation <will.gertler@gmail.com>
2023-02-24 14:21:36 -0500

    finish pre-processing

--- preProcessing.r
+++ preProcessing.r
@@ -3,6 +3,11 @@
 library(readODS)
 library(dplyr)
 
+#We set the working directory to that from where we are loading the initial
+#dataframes
+
+setwd("~/gitrepos/student-data-analysis/demodata/")
+
 
 #import attendance records
 classOne_attendance <- read_ods(path="~/gitrepos/student-data-analysis/demodata/1x-att_Original.ods",sheet=1)
@@ -29,4 +34,25 @@ classOne_attendance[] <- lapply(classOne_attendance, gsub, pattern="P (1/1)", re
 classTwo_attendance[] <- lapply(classTwo_attendance, gsub, pattern="A (0/1)", replacement="0", fixed=TRUE)
 classTwo_attendance[] <- lapply(classTwo_attendance, gsub, pattern="P (1/1)", replacement="1", fixed=TRUE)
 
+#Now we remove the extraneous rows from the grade data. This cannot be done in
+#the same way as there is no general identifying string indicating a dropped
+#course or late registration. The code below identifies the row difference by
+#surname, which is included in both frames. It then removes the difference.
 
+#There may be a "cleaner" way to perform this operation than the following, but
+#the below is quite readable and rather simple.
+
+diff_classOne <- anti_join(classOne_grades, classOne_attendance, by="Surname")
+classOne_grades <- classOne_grades[!(classOne_grades$Surname %in% diff_classOne$Surname),]
+
+diff_classTwo <- anti_join(classTwo_grades, classTwo_attendance, by="Surname")
+classTwo_grades <- classTwo_grades[!(classTwo_grades$Surname %in% diff_classTwo$Surname),]
+
+#Having completed our pre-processing, we save the files to our working
+#directory
+
+save(classOne_attendance, file="classOne_attendance.ods")
+save(classOne_grades, file="classOne_grades.ods")
+
+save(classTwo_attendance, file="classTwo_attendance.ods")
+save(classTwo_grades, file="classTwo_grades.ods")
